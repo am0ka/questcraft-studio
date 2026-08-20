9f3bb611855323308d6615691ceaea57dc7f2838
Amir Sarsen <amir@sarsen.dev>
2026-08-20 12:38:12 +0500

    build: add lines for coolify

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 const nextConfig: NextConfig = {
   output: 'standalone',
   outputFileTracingRoot: path.join(__dirname, '../../'),
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
